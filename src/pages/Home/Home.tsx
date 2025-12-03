9104240a2cfb9bf93281f77fbf720db3ea4a1804
Paulo Baltieri <37188572+paulobaltieri@users.noreply.github.com>
2025-12-03 17:59:27 -0300

    Correção titulo do video

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,8 @@ import Button from "@mui/material/Button";
 
 function Home() {
     // Texto presente na frente dos video
-    const titleVideo = "Bem vindo!"
+    const titleVideo = "Encontre todas as "
+    const titleVideo2 = "Competições de Jiu-Jitsu em um só lugar"
     const subtitleVideo = "Jiu-Jitsu Eventos — Os melhores campeonatos"
     return (
         <Box
@@ -47,13 +48,16 @@ function Home() {
                     flexDirection: "column",
                     color: "#fff",
                     textShadow: "2px 2px 10px rgba(0,0,0,0.8)",
+                    margin: "5px"
                     // pointerEvents: "none",  <-- REMOVIDO
                 }}
             >
-                <Typography variant="h2" fontWeight="bold">
-                    {titleVideo}
+                <Typography variant="h3" fontWeight="bold">
+                    <Box>{titleVideo}</Box>
                 </Typography>
-
+                <Typography variant="h3" fontWeight="bold">
+                    {titleVideo2}
+                </Typography>
                 <Typography variant="h5" mt={2} mb={3}>
                     {subtitleVideo}
                 </Typography>
